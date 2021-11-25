7400279f5cb9efd4716c10433129b37d7d01bb2e
Andrew Bernklau <andrewbernklau@hotmail.com>
2021-11-24 17:30:21 -0700

    Cleaned notebook by created function from code and adding them to files within code folder

---
+++ code/auth_and_scrape.py
@@ -0,0 +1,88 @@
+import tweepy
+import csv
+import time
+
+# Authentication
+def authenticate(consumer_key, consumer_secret, access_token, access_secret):
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    return tweepy.API(auth)
+
+def append_to_csv(tweets, fileName):
+
+    #A counter variable
+    counter = 0
+
+    #Open OR create the target CSV file
+    csvFile = open(fileName, "a", newline="", encoding='utf-8')
+    csvWriter = csv.writer(csvFile)
+
+    #Loop through each tweet
+    for tweet in tweets:
+        
+        # variable created for each aspect since some of the keys might not exist for some tweets
+        # So we will account for that
+
+        # 1. ID
+        id = tweet.id
+
+        # 2. Time created
+        created_at = tweet.created_at
+
+        # 4. Language
+        lang = tweet.lang
+
+        # 5. Tweet metrics
+        retweet_count = tweet.retweet_count
+        like_count = tweet.favorite_count
+
+        # 6. Tweet text
+        text = tweet.text
+        
+        # Assemble all data in a list
+        res = [id, created_at, lang, like_count, retweet_count, text]
+        
+        # Append the result to the CSV file
+        csvWriter.writerow(res)
+        counter += 1
+
+    # When done, close the CSV file
+    csvFile.close()
+
+    # Print the number of tweets for this iteration
+    print("# of Tweets added from this response: ", counter)
+
+
+def scrape_and_place(api):
+
+    #Ask for keyword with which to scrape.
+    keyword = input('Please enter keyword or hastag to search: ')
+
+    #Ask for number of tweets desired to scrape.
+    tweet_num = int(input('Please enter how many tweets to analyze (If greater than 900, must be multiple of 900): '))
+
+    #Ask for date at which access was requested as the scrape can only occur on tweets made within seven days prior to requested date.
+    date_req = input('Please enter the date from which you request YYYY/MM/DD: ')
+    date_req = date_req.split('/')
+
+    j = 0
+
+    #Only 900 tweets can be scraped every 15min so if desired amount is greater than 900 this will create a wait time to pull every 15 min.
+    if tweet_num > 900:
+
+        for i in range(0, tweet_num//900):
+            if j == 7:
+                break
+            elif j != 7:
+                tweets = tweepy.Cursor(api.search_tweets, q=keyword+' lang:en -filter:retweets', until=date_req[0] + '-' + date_req[1] + '-' + str((int(date_req[2]) - j))).items(900)
+            j += 1
+            append_to_csv(tweets, fileName='data.csv')
+            time.sleep(900)
+
+    elif tweet_num <= 900 & tweet_num > 0:
+        
+        for i in range(0, tweet_num):
+            tweets = tweepy.Cursor(api.search_tweets, q=keyword+' lang:en -filter:retweets', until=date_req[0] + '-' + date_req[1] + '-' + date_req[2]).items(tweet_num)
+            append_to_csv(tweets, fileName='data.csv')
---
+++ code/preparation.py
@@ -0,0 +1,114 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import sent_tokenize
+from nltk import word_tokenize
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import re
+from textblob import TextBlob
+
+
+
+def top_n_gram(corpus,ngram_range,n=None):
+
+    #Vectorize corpus
+    vec = CountVectorizer(ngram_range=ngram_range, stop_words='english', max_df=1.0, min_df=0.01).fit(corpus)
+
+    #creates bag of words and finds the sums of words
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    
+    #creates a list of words and their frequencies
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+
+    return words_freq[:n], bag_of_words
+
+
+def top_n_gram_test(corpus, trained_corpus, ngram_range,n=None):
+
+    #Vectorize corpus
+    vec = CountVectorizer(ngram_range=ngram_range, stop_words='english', max_df=1.0, min_df=0.01).fit(trained_corpus)
+
+    #Create bag of words and the sums
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+
+    #Create a list of words and their frequencies
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n], bag_of_words
+
+#Function to clean tweets of urls, punctuation and uppercase letters
+def clean_tweets(dataframe):
+
+    #Lambda function to remove urls from tweets providing links
+    remove_url = lambda tweet: re.sub(r'http\S+', '', tweet)
+
+    #Lambda function to remove punctuation from tweets
+    remove_punct = lambda tweet: re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+://\S+)"," ", tweet)
+
+    #Remove urls and punctuation from text column
+    dataframe["clean_text"] = dataframe.text.map(remove_url).map(remove_punct)
+
+    #Reduce text to lowercase format
+    dataframe["clean_text"] = dataframe.clean_text.str.lower()
+
+    return dataframe
+
+#Calculating Negative, Positive 
+def create_sentiment(dataframe):
+    df_probs = pd.DataFrame()
+
+    #create dataframe of subjectivity and subjectivity polarity of tweets
+    df_probs[['polarity', 'subjectivity']] = dataframe['clean_text'].apply(lambda text: pd.Series(TextBlob(text).sentiment))
+
+    #Scores each tweet based on the subjectivity polarity
+    for index, row in dataframe['text'].iteritems():
+
+        #Provides negative, neutral, positive and compound scores for the tweet in that row
+        score = SentimentIntensityAnalyzer().polarity_scores(row)
+
+        #extracts negative and positive scores
+        neg = score['neg']
+        pos = score['pos']
+        
+        #creates binary values based on which sentiment score is higher 
+        if neg > pos:
+            df_probs.loc[index, 'sentiment'] = 0
+        elif pos > neg:
+            df_probs.loc[index, 'sentiment'] = 1
+        else:
+            df_probs.loc[index, 'sentiment'] = 0
+            
+    return df_probs
+
+#Creates a dataframe of the positive tweets and matches their sentiment values
+def create_pos_df(dataframe, dataframe_probs):
+
+    #Grabs text from original dataframe 
+    dataframe.reset_index(drop=True, inplace=True)
+    df_pos = dataframe[['text']].copy(deep=True)
+
+    #Grabs sentiment values from probs dataframe
+    dataframe_probs.reset_index(drop=True, inplace=True)
+    df_pos['sentiment'] = dataframe_probs['sentiment'].copy(deep=True)
+
+    #Creates dataframe of only positive sentiment tweets
+    df_pos = df_pos[df_pos['sentiment'] == 1]
+    
+    return df_pos
+
+def create_features(text, top_words):
+    features = {}
+    wordcount = 0
+
+    #Adds 1 to wordcount if word in tokenized text is within the top list of words
+    for sentence in sent_tokenize(text):
+        for word in word_tokenize(sentence):
+            if word.lower() in top_words:
+                wordcount += 1
+
+    #adds wordcount to dictionary
+    features["wordcount"] = wordcount
+
+    return features['wordcount']
